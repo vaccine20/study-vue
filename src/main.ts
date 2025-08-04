5534a797dbe8889c6cd09e24fc6bb54f40d8c080
Sun Kim <rhodla83@gmail.com>
2025-08-04 16:54:11 +0900

    vuetify 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,19 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import './style.css'
 import App from './App.vue'
+
+// vuetify
+import "vuetify/styles"
+import '@mdi/font/css/materialdesignicons.min.css';
 import { createVuetify } from 'vuetify';
 import * as components from "vuetify/components";
-import 'vuetify/styles'
+import * as directives from 'vuetify/directives';
 
 const pinia = createPinia();
 const app = createApp(App);
 const vuetify = createVuetify({
     components,
+    directives,
 });
 
 app.use(pinia).use(vuetify).mount('#app');
